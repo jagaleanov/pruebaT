37016aacdd813e7effb92153983d908a1bdfbccb
Jorge G <eldelaprogramacion@gmail.com>
2024-02-08 00:55:40 -0500

    add api category controller

---
+++ laravel/app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryCollection;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+
+/**
+ * @OA\Tag(
+ *     name="Categories",
+ *     description="Operations about categories"
+ * )
+ */
+class CategoryController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/categories",
+     *     tags={"Categories"},
+     *     summary="List all categories",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Category")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error"
+     *     )
+     * )
+     */
+    public function index()
+    {
+        try {
+            $categories = Category::all();
+            // return $categories;
+            return CategoryCollection::collection($categories);
+        } catch (\Exception $e) {
+            Log::error("Error retrieving categories: {$e->getMessage()}");
+            return response()->json(['message' => 'Failed to retrieve categories'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/categories",
+     *     tags={"Categories"},
+     *     summary="Create a new category",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(ref="#/components/schemas/Category")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Category created",
+     *         @OA\JsonContent(ref="#/components/schemas/Category")
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error"
+     *     )
+     * )
+     */
+    public function store(CategoryRequest $request)
+    {
+        try {
+            $category = Category::create($request->all());
+            // return $category;
+            return new CategoryResource($category);
+        } catch (\Exception $e) {
+            Log::error('Category store error: ' . $e->getMessage());
+            return response()->json(['message' => 'Failed to save category'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/categories/{id}",
+     *     tags={"Categories"},
+     *     summary="Get a category by ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the category to return",
+     *         required=true,
+     *         @OA\Schema(type="integer", format="int64")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/Category")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Category not found"
+     *     )
+     * )
+     */
+    public function show(Category $category)
+    {
+        try {
+            $category = $category->load('articles');
+            return new CategoryResource($category);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to retrieve category'], Response::HTTP_INTERNAL_SERVER_ERROR); // 500 Internal Server Error
+        }
+    }
+
+    /**
+     * @OA\Put(
+     *     path="/api/categories/{id}",
+     *     tags={"Categories"},
+     *     summary="Update an existing category",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the category that needs to be updated",
+     *         @OA\Schema(type="integer", format="int64")
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(ref="#/components/schemas/Category")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Category updated",
+     *         @OA\JsonContent(ref="#/components/schemas/Category")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Category not found"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error"
+     *     )
+     * )
+     */
+    public function update(CategoryRequest $request, string $id)
+    {
+        try {
+            $category = Category::find($id);
+            if (!$category) {
+                return response()->json(['message' => 'Category not found'], Response::HTTP_NOT_FOUND); // 404 Not Found
+            }
+            $category->update($request->all());
+            // return $category;
+            return new CategoryRequest($category);
+        } catch (\Exception $e) {
+            Log::error("Error updating category: {$e->getMessage()}");
+            return response()->json(['message' => 'Failed to update category'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @OA\Delete(
+     *     path="/api/categories/{id}",
+     *     tags={"Categories"},
+     *     summary="Delete a category",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the category to delete",
+     *         @OA\Schema(type="integer", format="int64")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="No Content",
+     *         @OA\JsonContent()
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Category not found"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error"
+     *     )
+     * )
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $category = Category::find($id);
+            if (!$category) {
+                return response()->json(['message' => 'Category not found'], Response::HTTP_NOT_FOUND); // 404 Not Found
+            }
+            $category->delete();
+            return response()->json(null, Response::HTTP_NO_CONTENT); // 204 No Content
+        } catch (\Exception $e) {
+            Log::error("Error deleting category: {$e->getMessage()}");
+            return response()->json(['message' => 'Failed to delete category'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
