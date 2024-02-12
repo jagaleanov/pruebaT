ca52ed8be42cd3f5cb62527802a3e9bb29043fa1
Jorge G <eldelaprogramacion@gmail.com>
2024-02-12 10:09:34 -0500

    add role to user model, resource & scheme

--- laravel/app/Swagger/Schemas/UserSchema.php
+++ laravel/app/Swagger/Schemas/UserSchema.php
@@ -35,7 +35,8 @@ namespace App\Swagger\Schemas;
  *     properties={
  *         @OA\Property(property="id", type="integer", format="int64", description="User ID"),
  *         @OA\Property(property="name", type="string", description="User name"),
- *         @OA\Property(property="email", type="string", format="email", description="User email")
+ *         @OA\Property(property="email", type="string", format="email", description="User email"),
+ *         @OA\Property(property="role", type="string", description="User role")
  *     }
  * )
 */
