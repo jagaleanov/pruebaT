1df940a99cf295fdc1808ac7cb666782e0a79930
Jorge G <eldelaprogramacion@gmail.com>
2024-02-15 05:42:01 -0500

    add image upload to swagger

--- laravel/app/Http/Controllers/Api/ImageController.php
+++ laravel/app/Http/Controllers/Api/ImageController.php
@@ -5,8 +5,56 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Tag(
+ *     name="Images",
+ *     description="Upload images"
+ * )
+ */
 class ImageController extends Controller
-{
+{/**
+     * @OA\Post(
+     *     path="/api/images",
+     *     tags={"Images"},
+     *     summary="Upload an image",
+     *     operationId="uploadImage",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Image file to upload",
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="file",
+     *                     description="Image file",
+     *                     type="string",
+     *                     format="binary",
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful upload",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="location",
+     *                     type="string",
+     *                     description="URL of the uploaded image",
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid request",
+     *     ),
+     * )
+     */
     public function upload(Request $request)
     {
         if ($request->hasFile('file')) {
