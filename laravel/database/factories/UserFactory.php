a200d755a9030834e151a3c770c25c1d8d932284
Jorge G <eldelaprogramacion@gmail.com>
2024-02-15 03:01:16 -0500

    add seeders

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -23,12 +23,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $roles = ['user', 'editor', 'admin'];
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => $roles[array_rand($roles)],
         ];
     }
 
