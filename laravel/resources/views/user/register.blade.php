b1f4b3d375d3b595a02423ed77721a3b9d63d7d5
Jorge G <eldelaprogramacion@gmail.com>
2024-02-15 04:00:56 -0500

    register view

--- laravel/resources/views/user/register.blade.php
+++ laravel/resources/views/user/register.blade.php
@@ -1,6 +1,23 @@
-<!-- resources/views/auth/register.blade.php -->
-<form method="POST" action="{{ route('register') }}">
-    @csrf
-    <!-- Campos de formulario: nombre, email, password, confirmación de password -->
-    <button type="submit">Register</button>
-</form>
+@extends('layout.default')
+@section('content')
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" name="name" id="name" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" name="email" id="email" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password_confirmation" class="form-label">Password confirmation</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@endsection
